5607399b7679eb08a6e463bca640e5602811f3e1
Sushant Choudhary <schoudhary@atlassian.com>
2015-10-05 11:38:00 +0700

    Initial Commit

---
+++ UITestDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITestDemo
+//
+//  Created by Sushant Choudhary on 10/5/15.
+//  Copyright © 2015 Sushant Choudhary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
